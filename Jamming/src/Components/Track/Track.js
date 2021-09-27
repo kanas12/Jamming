da712a0d927b15abe0bc1a1fbea84e508c9fe334
Daniel Frimpong <daniel.frimpong@amlitech.org>
2021-09-27 16:54:26 +0000

    first commit

---
+++ Jamming/src/Components/Track/Track.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Track.css";
+
+export class Track extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.renderAction = this.renderAction.bind(this);
+  }
+
+  addTrack() {
+    this.props.onAdd(this.props.track);
+  }
+
+  removeTrack() {
+    this.props.onRemove(this.props.track, true);
+  }
+
+  renderAction() {
+    if (this.props.isRemoval) {
+      this.removeTrack();
+    } else {
+      this.addTrack();
+    }
+  }
+
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>{this.props.name}</h3>
+          <p>
+            {this.props.artist} | {this.props.album}
+          </p>
+        </div>
+        <button className="Track-action" onClick={this.renderAction}>
+          +
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Track;
